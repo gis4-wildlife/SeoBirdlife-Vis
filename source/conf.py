a9e19b97bd29f50003a449eb884ec527b051816b
bryanvallejo16 <bryanvallejo16@gmail.com>
2023-06-22 22:55:51 +0300

    inicio

---
+++ source/conf.py
@@ -0,0 +1,117 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'SeoBirdlife - Vizualization "Migra" Project'
+copyright = '2023, GIS4 Wildlife Movement Analytics'
+author = 'GIS4 Wildlife | bryan@gis4-wildlife.com'
+
+# The full version, including alpha/beta/rc tags
+release = 'v1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.mathjax',
+    'sphinx.ext.todo',
+    'sphinx.builders.linkcheck',
+    'sphinx_togglebutton',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive',
+    'myst_nb',
+    # 'jupyter_sphinx',
+    # 'sphinx_tabs.tabs',
+    "sphinx_inline_tabs",
+   # 'sphinx_last_updated_by_git'
+]
+
+# Suffid=x to files m2r
+source_suffix = ['.rst', '.md']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+html_logo = '_static/wt-web-logo.png'
+html_title = "SeoBirdlife - GIS4 Wildlife"
+
+# navigation bar elements on left
+html_sidebars = {
+    "**": ["navbar-logo.html", "search-field.html", "sbt-sidebar-nav.html"]
+}
+
+# True, otherwise collapse the content of all page
+sphinx_tabs_disable_tab_closing = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/wt.css'
+]
+
+html_theme_options = {
+    # "external_links": [],
+    "toc_title": "",
+    "home_page_in_toc": True,
+    "navbar_end": ["navbar-icon-links"],
+    "repository_url": "https://github.com/gis4-wildlife/SeoBirdlife-Vis",
+    "repository_branch": "main",
+    "path_to_docs": "source/",
+    # "twitter_url": "https://twitter.com/pythongis",
+    # "google_analytics_id": "",
+    "use_edit_page_button": False,
+    "use_repository_button": False,
+    "launch_buttons": {
+        "binderhub_url": "https://notebooks.gesis.org/binder",
+        "thebe": False,
+        "notebook_interface": "jupyterlab",
+        "collapse_navigation": False,
+        # Google Colab does not provide an easy way for specifying/building/activating the conda environment
+        # in a similar manner as Binder. Hence, let's not keep it. The easiest way seems to be:
+        # https://github.com/jaimergp/condacolab
+        # But it requires actions from the user nontheless, so atm it's a no-go.
+        #"colab_url": "https://colab.research.google.com"
+    },
+}
+
+# Allow errors
+# execution_allow_errors = True
+
+# Do not execute cells
+# jupyter_execute_notebooks = "off"
---
+++ source/index.rst
@@ -0,0 +1,109 @@
+**Inicio** 
+~~~~~~~~~~~~~~~~~~~~
+
+.. tab:: ¿Qué es el "Wildlife Tracker"?
+    
+  .. raw:: html
+
+    <style>
+        div.tabs {
+          width: 90%;
+          text-align: justify;
+            }
+    </style>
+
+    <div class='tabs'>
+      <p><br>
+        Es un framework para visualización geográfica y segmentación espacio-temporal 
+        de datos obtenidos con tags satelitales instalados en vida salvaje.
+        Tiene la capacidad de actualizar en tiempo real las localizaciones 
+        de tags activos y también puede ser desarrollado como un producto estático 
+        de visualizacion para tags que ya dejaron de enviar señales al satellite.    
+      </p>
+    </div>
+
+
+.. tab:: ¿Para qué sirve esta documentación?
+    
+  .. raw:: html
+
+
+    <div class='tabs'>
+      <p>
+        <br>Esta web fue creada para instruir a los usuarios del "Wildlife Tracker" 
+        sobre las capacidades de su visualización dinámica e interactiva.
+        Contiene tutoriales para entender cada función específica en la exploración 
+        visual de datos geoespaciales obtenidos con tags satellitales instalados en 
+        ejemplares de vida salvaje.      
+      
+      </p>
+    </div>
+
+
+.. tab:: ¿Cómo uso esta web?
+
+  .. raw:: html
+
+    <div class='tabs'>
+      <p><br>
+        Cada tutorial utiliza como ejemplo el web map desarrollado con datos del estudio  
+        <i>"Egyptian Vulture in Spain - Migra Program"</i> perteneciente a <a href="https://seo.org/" target="_blank">SEO BirdLife</a>. 
+        Se recomienda interactuar con el web map siguiendo las indicaciones de cada 
+        tutorial de manera que el usuario pueda descubrir 
+        las potencialidades del "Wildlife Tracker" y a aprender a usarlo 
+        para generar conocimiento en base a la exploracion visual de datos.<br>
+
+      </p>
+    
+    
+    </div>
+
+
+.. raw:: html 
+
+  <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 d-flex">
+                <div class="card text-center intro-card shadow">
+                  <img src="_static/wt-icon-web.png" class="card-img-top" alt="Wildlife Tracker" >
+                    <div class="card-body flex-fill">
+                      <h5 class="card-title"><b>GIS4 Wildlife</b></h5>
+                        <p class="card-text">Descubre el poder del SIG en la nube y el analisis del movimiento en tiempo real con tags satelitales. Visita nuestra web.</p>
+
+.. container:: custom-button
+
+  .. raw:: html
+
+    <a href="https://www.gis4-wildlife.com/" target="_blank">www.gis4-wildlife.com</a>
+    
+.. raw:: html
+
+                    </div>
+              </div>
+            </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 d-flex">
+              <div class="card text-center intro-card shadow">
+              <img src="_static/seobirdlife-logo.png" class="card-img-top" alt="seobirdlife">
+              <div class="card-body flex-fill">
+                  <h5 class="card-title"><b>SEO BirdLife Demo</b></h5>
+                  <p class="card-text">Interactua con las capacidades de los web maps de SEO BirdLife para entender a fondo la migración y comportamiento de aves migratorias.</p>
+
+.. container:: custom-button
+
+    `Egyptian Vultures Demo <https://www.gis4-wildlife.com/>`__
+
+.. raw:: html
+
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+      
+
+
+
+
+
+
+
